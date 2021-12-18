a0ffcf68516989deabac60382b017ad0043b443a
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-12-18 02:27:52 +0100

    updates

--- components/Property.jsx
+++ components/Property.jsx
@@ -6,7 +6,7 @@ import { BsGridFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import millify from "millify";
 
-import DefaultImage from "../assets/images/house.jpg";
+import DefaultImage from "../assets/images/house.png";
 
 const Property = ({
   property: {
