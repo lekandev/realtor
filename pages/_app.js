bab51a5a4fc62fb1dd8713174a7c7816b7dc9265
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-12-18 02:20:34 +0100

    Commented out nprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import Router from "next/router";
 import Head from "next/head";
-import NProgress from "nprogress";
+// import NProgress from "nprogress";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import Layout from "../components/Layout"
